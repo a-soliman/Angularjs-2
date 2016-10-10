61f8f75eea8a55bee7b0ab6379bdc064f4835a16
Ahmed Soliman <ahmedanwar@mail.com>
2016-10-09 22:42:06 -0700

    understanding component and its method of relaying on the styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+	h1 {
+		color: red;
+	}
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'This is my App!';
 }
