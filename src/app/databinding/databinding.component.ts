70457310c134ce6d2f180746a2eb7be3f2dd8be9
Ahmed Soliman <ahmedanwar@mail.com>
2016-10-11 20:25:35 -0700

    created a new component dataBinding

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+	stringInterpolation = 'this is a sting interpolation';
+	numberInterpolation = 2;
+}
