9caf7846a176ae03ef7ac45e72d4a7886c25d9ff
Ahmed Soliman <ahmedanwar@mail.com>
2016-10-11 20:22:52 -0700

    modified the code and removed the other component and its import lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  	<h1>Inline Template</h1>
-  	<fa-other></fa-other>
-  	<fa-another>
-		<div>
-			<h1>Hello!</h1>
-			<p>World!</p>
-		</div>
-  	</fa-another>
-  	<fa-another>
-		<p>Something else</p>
-  	</fa-another>
-  	`,
-  styles: [`
-	h1 {
-		color: red;
-	}
-  `]
+  	<h1>Root Component</h1>
+  	<app-databinding></app-databinding>
+  	`
 })
 export class AppComponent {
   title = 'This is my App!';
